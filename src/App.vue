83bf78942253696ef4f826c4607d2a39679843ea
nwemar <capital.nwemarwin@gmail.com>
2022-01-06 16:30:36 +0630

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Hello Wrold"/>
+    <first msg="My first component project" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import first from './components/first.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    first
   }
 }
 </script>
